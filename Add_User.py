bebc602ba43a5ae245e77cd2de33f5d91a137093
Kenner <kennerskindle@gmail.com>
2025-07-17 14:11:09 -0400

    initial commit

---
+++ Add_User.py
@@ -0,0 +1,10 @@
+import sqlite3
+import pandas as pd
+import os
+import shutil
+from datetime import datetime,time
+import Shira
+from Shira import Newest_file
+
+
+
---
+++ Remove_User.py
@@ -0,0 +1,7 @@
+import sqlite3
+import pandas as pd
+import os
+import shutil
+from datetime import datetime,time
+import Shira
+from Shira import Newest_file
